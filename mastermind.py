ff8c368ba972ec3f5eb91eb9dcf712f413ca9c14
Russ Paul-Jones <russ@paul-jones.org>
2025-05-17 17:27:08 -0700

    Add command line parameters and standard colors

--- mastermind.py
+++ mastermind.py
@@ -11,16 +11,29 @@ from time import process_time
 
 app_name = 'mastermind.py'
 
+standard_colors = [
+    "R",
+    "B",
+    "Y",
+    "G",
+    "P",
+    "W"
+]
+
 
 def main(arguments):
     program_name = app_name
-    command_line_documentation = f'{program_name} --help --verbose --section [a|b] --file [input file]'
+    command_line_documentation = f'{program_name} --help --verbose --player [Person|Computer] --colors num_colors --dots num_dots'
     verbose = False
+    player = 'P'
+    num_colors = 6
+    num_dots = 4
     input_file_name = ''
     sections = []
 
     try:
-        opts, args = getopt(arguments, "hvs:f:", ("help", "verbose", "section=", "file="))
+        opts, args = getopt(arguments, "hvp:c:d:", 
+                            ("help", "verbose", "player=", "colors=", "dots="))
     except GetoptError:
         print(f'Invalid Arguments: {command_line_documentation}')
         exit(2)
@@ -33,22 +46,31 @@ def main(arguments):
         if opt in ('-v', '--verbose'):
             verbose = True
 
-        if opt in ('-f', '--file'):
-            input_file_name = arg
+        if opt in ('-p', '--player'):
+            if arg[0] == 'c' or arg[0] == 'C':
+                player = 'C'
 
-        if opt in ('-s', '--section'):
-            for section in arg:
-                sections.append(section)
+        if opt in ('-c', '--colors'):
+            num_colors = int(arg)
+            if num_colors > 26:
+                num_colors = 26
+
+        if opt in ('-d', '--dots'):
+            num_dots = int(arg)
 
     if input_file_name:
         with open(input_file_name, 'r') as input_file:
             print(f'Opened {input_file_name} for {app_name}')
 
     time_start = process_time()
-    for section in sections:
-        print(f'Processing section {section}')
+    print(f'{player} will play with {num_dots} dots of {num_colors} colors.')
+    if num_colors <= 6:
+        colors = standard_colors[:num_colors]
+    else:
+        colors = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")[:num_colors]
+
     if verbose:
-        print('Debugging output goes here')
+        print(f'The colors are {colors}')
     time_end = process_time()
     print(f'Time taken: {time_end - time_start} seconds.')
 
